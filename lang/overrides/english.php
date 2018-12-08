6336299ee4056a8ce1c3f05a08ef6daeb0419d9c
Elesei <elesei@elesei.com>
2018-12-08 10:57:04 +0200

    add translate overrides

---
+++ lang/overrides/english.php
@@ -0,0 +1,5 @@
+<?php
+
+$_LANG['domainsautorenewradiooff'] = 'Off';
+$_LANG['domainsautorenewradioon'] = 'On';
+$_LANG['domainsautorenewradiodescription'] = 'If you do not need to renew a domain in automatic mode, you can select the appropriate value.<br/>By default, this option is enabled for all new domains.';
